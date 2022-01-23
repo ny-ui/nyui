df073d8f78fdb9e7acab7cbd3eb1a126173f3ac8
tunguyen <tunguyen@chotot.vn>
2022-01-23 17:44:30 +0700

    next

--- packages/nyui-core/theme/index.js
+++ packages/nyui-core/theme/index.js
@@ -1,10 +1,19 @@
-import defaultTheme from './defaultTheme';
+import defaultTheme from './default-theme'
+
+//  const result = resolveConfig([userConfig, defaultConfig])
 
 const resolveTheme = () => {
-  return defaultTheme
+  let userTheme = {}
+  // console.log(path.resolve('./nyui.config.js'))
+  if (typeof window === 'undefined') {
+    userTheme.primary = '#dc2626'
+    console.log(require.main.filename)
+    console.log(__dirname, 'dirname')
+    console.log('find nyui.config.js')
+  }
+  return { ...defaultTheme, ...userTheme }
 }
 
 const theme = resolveTheme()
 
 export default theme
-
